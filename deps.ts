5b1645e4f3aae836f8c17db2cbd48ff3b54e877d
Qu4k <pippo.reds@gmail.com>
2020-07-10 20:32:07 +0200

    chore: make deps readable

--- deps.ts
+++ deps.ts
@@ -1,11 +1,5 @@
-export { Command } from "https://deno.land/x/cliffy@v0.9.0/command.ts";
-export {
-  Input,
-  Confirm,
-  List,
-} from "https://deno.land/x/cliffy@v0.9.0/prompt.ts";
-export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
 export * as path from "https://deno.land/std@v0.56.0/path/mod.ts";
+
 export {
   existsSync,
   expandGlobSync,
@@ -13,30 +7,47 @@ export {
   readJson,
   exists,
 } from "https://deno.land/std@v0.56.0/fs/mod.ts";
+
 export {
   bold,
   green,
   yellow,
   red,
 } from "https://deno.land/std@v0.56.0/fmt/colors.ts";
+
 export {
   assertEquals,
   assertMatch,
   assert,
 } from "https://deno.land/std@v0.56.0/testing/asserts.ts";
+
 export {
   parse,
   stringify,
 } from "https://deno.land/std@0.56.0/encoding/yaml.ts";
+
+export { Command } from "https://deno.land/x/cliffy@v0.9.0/command.ts";
+
+export {
+  Input,
+  Confirm,
+  List,
+} from "https://deno.land/x/cliffy@v0.9.0/prompt.ts";
+
+export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+
 export * as base64 from "https://denopkg.com/chiefbiiko/base64@v0.2.0/mod.ts";
+
 export {
   installUpdateHandler,
   globalModulesConfigPath,
 } from "https://x.nest.land/eggs-update-handler@0.5.1/mod.ts";
+
 export {
   getLatestVersionFromNestRegistry,
   getLatestVersion,
   analyzeURL,
   versionSubstitute,
 } from "https://x.nest.land/eggs-update-handler@0.5.1/lib/registries.ts";
+
 export const lstatSync = Deno.lstatSync;
