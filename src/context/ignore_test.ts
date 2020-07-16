4e26a891b545a00060aa84792920024b29ba7060
Qu4k <pippo.reds@gmail.com>
2020-07-16 18:07:38 +0200

    chore: format code

--- src/context/ignore_test.ts
+++ src/context/ignore_test.ts
@@ -12,10 +12,16 @@ test/*
     # this is a comment, just a bit indented
     `);
     if (Deno.build.os === "windows") {
-      assertEquals(matched.denies, [/^\.git(?:\\|\/)(?:[^\\/]*)$/, /^test(?:\\|\/)(?:[^\\/]*)$/]);
+      assertEquals(
+        matched.denies,
+        [/^\.git(?:\\|\/)(?:[^\\/]*)$/, /^test(?:\\|\/)(?:[^\\/]*)$/],
+      );
       assertEquals(matched.accepts, [/^test\\should_keep_this\.ts$/]);
     } else {
-      assertEquals(matched.denies, [/^\.git\/(?:[^//]*)$/, /^test\/(?:[^//]*)$/]);
+      assertEquals(
+        matched.denies,
+        [/^\.git\/(?:[^//]*)$/, /^test\/(?:[^//]*)$/],
+      );
       assertEquals(matched.accepts, [/^test\/should_keep_this\.ts$/]);
     }
   },
