467417f061b7b378f40a84e5d7cee82d33c3b0b8
divy-work <dj.srivastava23@gmail.com>
2020-08-13 23:02:59 +0530

    chores: update endpoint

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,12 @@
 export function envHOMEDIR(): string {
   return (
     Deno.env.get("HOME") ??
-      Deno.env.get("HOMEPATH") ??
-      Deno.env.get("USERPROFILE") ??
-      "/"
+    Deno.env.get("HOMEPATH") ??
+    Deno.env.get("USERPROFILE") ??
+    "/"
   );
 }
 
 export function envENDPOINT(): string {
-  return Deno.env.get("EGGS_ENDPOINT") ?? "http://localhost:8080";
-  // "https://api.nest.land";
+  return Deno.env.get("EGGS_ENDPOINT") ?? "https://api.nest.land";
 }
