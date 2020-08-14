3bd8270c3ed3aecd01425b86f0cbe8e1f5fd0668
divy-work <dj.srivastava23@gmail.com>
2020-08-14 11:02:43 +0530

    chores: fmt

--- src/environment.ts
+++ src/environment.ts
@@ -1,9 +1,9 @@
 export function envHOMEDIR(): string {
   return (
     Deno.env.get("HOME") ??
-    Deno.env.get("HOMEPATH") ??
-    Deno.env.get("USERPROFILE") ??
-    "/"
+      Deno.env.get("HOMEPATH") ??
+      Deno.env.get("USERPROFILE") ??
+      "/"
   );
 }
 
